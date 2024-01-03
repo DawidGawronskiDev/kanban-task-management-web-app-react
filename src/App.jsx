16b1d48c37743e8e38f0895c34949426e8b8bad2
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-01-03 20:28:16 +0000

    Fetch data and createContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import { createContext, useEffect, useState } from "react";
+import { redirect } from "react-router-dom";
+
+const AppContext = createContext({
+  data: {},
+  currentBoardName: "",
+});
+
 const App = () => {
-  return <span>Hello World!</span>;
+  const [data, setData] = useState(null);
+  const [currentBoardName, setCurrentBoardName] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const storedData = localStorage.getItem("appData");
+
+      if (storedData) {
+        setData(JSON.parse(storedData));
+      } else {
+        try {
+          const response = await fetch("/data.json");
+          const fetchedData = await response.json();
+
+          localStorage.setItem("data", JSON.stringify(fetchedData));
+          setData(fetchedData);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      setCurrentBoardName(data.boards[0].name);
+    }
+  }, [data]);
+
+  return (
+    <AppContext.Provider value={{ data, currentBoardName }}>
+      <span>Hello World!</span>
+    </AppContext.Provider>
+  );
 };
 
 export default App;
